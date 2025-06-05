8cced41370b2c1c3febfede01baeedb25475c93f
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-05 18:57:00 +0530

    misc storage account

---
+++ tf-state-init/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "4a491ea7-cd6d-4ec6-aa18-28b31973e70c"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfStorageState"
+  resource_group_name      = "devops_project_ecom"
+  location                 = "UK West"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "tfcontainer" {
+  name                  = "roboshop-tfstate-file"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
