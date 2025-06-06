fea9fbb216046bc56f7b6a205e5986b5e6196106
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-06 16:17:14 +0530

    depends on private ip

--- tools-setup/vm/main.tf
+++ tools-setup/vm/main.tf
@@ -25,6 +25,7 @@ resource "azurerm_network_interface_security_group_association" "nsg-allow" {
 }
 
 resource "azurerm_virtual_machine" "vm" {
+  depends_on                    = [ azurerm_network_interface.privateip ]
   name                          = var.name
   location                      = var.location
   resource_group_name           = var.rg_name
