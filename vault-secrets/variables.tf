9f79606e277de582ebef135545160e4f27f95b86
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-08 14:37:10 +0530

    roboshop 1

---
+++ vault-secrets/variables.tf
@@ -0,0 +1,21 @@
+variable "token" {}
+
+variable "secrets" {
+  default = {
+    roboshop-dev = {
+      description = "Roboshop app components all secrets"
+    }
+  }
+}
+
+variable "values" {
+  default = {
+    cart = {
+      secret = "roboshop-dev"
+      values = {
+        username = "harshal"
+        password = "harshal@12345"
+      }
+    }
+  }
+}
