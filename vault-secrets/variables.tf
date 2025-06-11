d68f1ef4ab3c8f51f106171739c3beedc9e8fba6
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-11 19:42:06 +0530

    docker ansible and terraform

--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -55,7 +55,7 @@ variable "values" {
         MONGO_URL   = "mongodb://mongodb-dev.harshaldevops.online:27017/catalogue"
         DB_TYPE     = "mongo"
         APP_GIT_URL = "https://github.com/roboshop-devops-project-v3/catalogue"
-        DB_HOST     = "mongo-ip"
+        DB_HOST     = "mongodb-dev.rdevopsb84.online"
         SCHEMA_FILE = "db/master-data.js"
       }
     }
